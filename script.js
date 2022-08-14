7f99a90947bd29471315d72b201b8eed640a9607
Abul Fajle Azad <abulfajle405@gmail.com>
2022-08-14 22:44:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Implement the function numOfWords, which should take a string
+and return its result as an integer which is the number of space seperated words in it (Use JS In built functions) */
+
+
+function numOfWords(str){
+    // write code here
+    // return the output , do not use console.log here
+    return str.split(" ").length;
+}
